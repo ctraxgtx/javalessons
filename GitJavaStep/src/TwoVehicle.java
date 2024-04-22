a1a89ce787eae2070e81a524a2c4a6540b0cfd47
ctrax <ctrax.gtx@gmail.com>
2024-04-22 14:56:34 +0200

    add class two vehicle

---
+++ GitJavaStep/src/TwoVehicle.java
@@ -0,0 +1,26 @@
+public class TwoVehicle {
+    public static void main(String[] args) {
+        Vehicle minivan = new Vehicle();
+        Vehicle sportcare = new Vehicle();
+
+        int range1, range2;
+
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        sportcare.passengers = 2;
+        sportcare.fuelcap = 14;
+        sportcare.mpg = 12;
+
+        range1 = minivan.fuelcap * minivan.mpg;
+        range2 = sportcare.fuelcap * sportcare.mpg;
+
+        System.out.println("Мини-фургон может перевезти " +
+                minivan.passengers + " пассажиров на расстояние " +
+                range1 + " миль.");
+        System.out.println("Спортивный автомобиль может перевезти "+
+                sportcare.passengers + " пассжиров на растояние "+
+                range2 + " миль.");
+    }
+}

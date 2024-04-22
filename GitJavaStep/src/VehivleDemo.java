1ca86172922779d6b3688411b113567c20e495a1
vadym <ctraxgtx@gmail.com>
2024-04-22 17:26:25 +0200

    Omen ProvaGit e VehivleDemo modifica

--- GitJavaStep/src/VehivleDemo.java
+++ GitJavaStep/src/VehivleDemo.java
@@ -10,5 +10,13 @@ public class VehivleDemo {
 
         System.out.println("minivan mozhet perevezti " + minivan.passengers + " " +
                 "passangers na rastojanie " + range + " milles ");
+
+        Vehicle car1 = new Vehicle();
+        car1.mpg = 25;
+        Vehicle car2 = car1;
+
+        System.out.println(car1.mpg);
+        System.out.println(car2.mpg);
+
     }
 }

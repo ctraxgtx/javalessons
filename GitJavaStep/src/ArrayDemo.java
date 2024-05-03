2b14d79fe1be7ac63e4509e6b61726faf939de2f
ctrax <ctrax.gtx@gmail.com>
2024-05-03 14:34:16 +0200

    Izuchenie Massivov

---
+++ GitJavaStep/src/ArrayDemo.java
@@ -0,0 +1,13 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] simple = new int[10];
+
+
+        for (int i = 0; i < 10; i++) {
+            simple[i] = i;
+        }
+        for (int i = 0; i < simple.length; i++) {
+            System.out.println("Элемент simple[" + i + "]: " + simple[i]);
+        }
+    }
+}
